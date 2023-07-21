fdbf432145340fa69354069dcbf52b15e4f9b974
Khalid-P20 <khalidhazzam20@gmail.com>
2023-07-20 17:44:35 -0700

    0x03. C - Debugging

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+void print_square(int size);
+void print_triangle(int size);
+
+#endif /* MAIN_H */
