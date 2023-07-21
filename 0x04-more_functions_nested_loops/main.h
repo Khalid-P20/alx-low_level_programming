cd5488eebb90530537763072836c4a1e3120c099
Khalid-P20 <khalidhazzam20@gmail.com>
2023-07-20 18:59:57 -0700

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,6 +7,7 @@ int _isdigit(int c);
 int mul(int a, int b);
 void print_square(int size);
 void print_numbers(void);
+void print_most_numbers(void);
 void print_triangle(int size);
 
 #endif /* MAIN_H */
