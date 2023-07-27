9c8f0a217d4980393bd45842d8b9dc4473508e59
Khalid-P20 <khalidhazzam20@gmail.com>
2023-07-27 19:19:26 +0200

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,26 +1,16 @@
 #include <stdio.h>
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
 int main(void)
+
 {
-	int a[5] = {0, 1, 2, 3, 4};
+	int n;
+	int a[5];
 	int *p;
 
-
-	p = &a[2];
-	*(p + 1) = 98; /* Add this line to modify the value of a[2] */
-
-
-	/* Printing the elements of the array */
-	printf("a[0] = %d\n", a[0]);
-	printf("a[1] = %d\n", a[1]);
+	a[2] = 1024;
+	p = &n;
+	/* Add the line of code here... */
+	*(p + 5) = 98;
+	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
-	printf("a[3] = %d\n", a[3]);
-	printf("a[4] = %d\n", a[4]);
-
 	return (0);
 }
